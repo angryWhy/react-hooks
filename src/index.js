2f7e429717ae88e37fa1c62ede96d043386ed7dd
angryWhy <875978822@qq.com>
2021-11-19 14:04:36 +0800

    11.19

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import Epp from"./components/09-ueslayouteffect/Ueslayouteffect"
+import Epp from"./components/09-ueslayouteffect/useEffect"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
